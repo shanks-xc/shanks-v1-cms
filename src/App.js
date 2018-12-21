d0f43d642f8ddd25a0482c9d5dcc407540568e29
shanks <574752304@qq.com>
2018-12-21 16:32:38 +0800

    开始时间12.21

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Button} from 'antd';
+import './App.css';
+import 'antd/dist/antd.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Button type="primary"> Button </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
